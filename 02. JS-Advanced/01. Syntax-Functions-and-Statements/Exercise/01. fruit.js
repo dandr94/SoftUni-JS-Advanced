5f37bcb7f3050a16cd6c62332edb49e51ccf1dea
dandr94 <d.andreev1994@gmail.com>
2022-05-19 18:24:00 +0300

    Update 01.fruits

--- 02. JS-Advanced/01. Syntax-Functions-and-Statements/Exercise/01. fruit.js
+++ 02. JS-Advanced/01. Syntax-Functions-and-Statements/Exercise/01. fruit.js
@@ -1,9 +1,10 @@
-function moneyNeededForFruit(fruit, grams, pricePerKg){
-    let kg = (grams / 1000).toFixed(2)
-    let finalSum = (kg * pricePerKg).toFixed(2)
-    console.log(`I need $${finalSum} to buy ${kg} kilograms ${fruit}.`)
+function moneyNeededForFruit(fruit, grams, pricePerKg) {
+  let kg = grams / 1000;
+  let finalSum = kg * pricePerKg;
+  console.log(
+    `I need $${finalSum.toFixed(2)} to buy ${kg.toFixed(2)} kilograms ${fruit}.`
+  );
 }
 
-
-moneyNeededForFruit('orange', 2500, 1.80)
-moneyNeededForFruit('apple', 1563, 2.35)
+moneyNeededForFruit("orange", 2500, 1.8);
+moneyNeededForFruit("apple", 1563, 2.35);
