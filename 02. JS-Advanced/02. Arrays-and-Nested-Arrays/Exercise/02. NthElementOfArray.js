add871baace3846f882ab8ae37650f95b82b7d36
dandr94 <d.andreev1994@gmail.com>
2022-05-23 16:00:49 +0300

    Update 02. from Arrays-and-Nested-Arrays

--- 02. JS-Advanced/02. Arrays-and-Nested-Arrays/Exercise/02. NthElementOfArray.js
+++ 02. JS-Advanced/02. Arrays-and-Nested-Arrays/Exercise/02. NthElementOfArray.js
@@ -1,34 +1,15 @@
-function solve(arr, n){
-    let result = []
+function solve(arr, n) {
+  let result = [];
 
-    for (let i = 0; i < arr.length; i++){
-        if (i % n == 0){
-            result.push(arr[i])
-        }
-    }
+  for (let i = 0; i < arr.length; i += n) {
+    result.push(arr[i]);
+  }
 
-    console.log(result)
+  return result;
 }
 
-solve(['5', 
-'20', 
-'31', 
-'4', 
-'20'], 
-2
-)
+solve(["5", "20", "31", "4", "20"], 2);
 
-solve(['dsa',
-'asd', 
-'test', 
-'tset'], 
-2
-)
+solve(["dsa", "asd", "test", "tset"], 2);
 
-solve(['1', 
-'2',
-'3', 
-'4', 
-'5'], 
-6
-)
+solve(["1", "2", "3", "4", "5"], 6);
